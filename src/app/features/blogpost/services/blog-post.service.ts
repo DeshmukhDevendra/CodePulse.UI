d2d0488813171ced77d71e3639700347521317de
Devendra Deshmukh <devendra.sharad.deshmukh@gmail.com>
2023-12-29 01:28:27 +0530

    Update and delete Blog Posts

--- src/app/features/blogpost/services/blog-post.service.ts
+++ src/app/features/blogpost/services/blog-post.service.ts
@@ -4,6 +4,7 @@ import { AddBlogPost } from '../models/add-blog-post.model';
 import { Observable } from 'rxjs';
 import { BlogPost } from '../models/blog-post.model';
 import { environment } from '../../../../environments/environment';
+import { UpdateBlogPost } from '../models/edit-blog-post.model';
 
 @Injectable({
   providedIn: 'root'
@@ -20,4 +21,15 @@ export class BlogPostService {
     return this.http.get<BlogPost[]>(`${environment.apiBaseUrl}/api/blogposts`);
   }
 
+  getBlogPostById(id:string): Observable<BlogPost>{
+    return this.http.get<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`);
+  }
+
+  updateBlogPost(id:string, model:UpdateBlogPost): Observable<BlogPost>{
+    return this.http.put<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`,model);
+  }
+
+  deleteBlogPost(id:string): Observable<BlogPost>{
+    return this.http.delete<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`);
+  }
 }
